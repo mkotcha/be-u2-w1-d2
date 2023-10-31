ff91fb29ec8b2811759cc31872a0898eab050385
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-31 14:30:31 +0100

    runner created

--- src/main/java/com/emmek/menu/MenuApplication.java
+++ src/main/java/com/emmek/menu/MenuApplication.java
@@ -10,7 +10,7 @@ public class MenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MenuApplication.class, args);
-        configurationClass();
+        
 
     }
 
